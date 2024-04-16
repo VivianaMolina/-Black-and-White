14f953be3f02cf854f064f7279d637d749680354
Viviana Molina <viviana.a.molina@gmail.com>
2024-04-16 19:14:34 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+import express from 'express';
+const app = express();
+import Jimp from 'jimp';
+import fs from 'fs';
+import { v4 as uuidv4 } from 'uuid';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PORT = 3000;
+
+
+app.listen(PORT, () => {
+    console.log(`El servidor está inicializado en el puerto ${PORT}`)
+});
+
+app.use(express.static("assets"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+});
+
+app.get("/imagen", async (req, res) => {
+    res.setHeader('Content-Type', 'image/jpeg')
+    const { userImagen } = req.query;
+    try {
+        const imagen = await Jimp.read(`${userImagen}`)
+        const imgId = uuidv4().slice(0,6);
+        await imagen
+            .resize(350, Jimp.AUTO)
+            .greyscale()
+            .writeAsync(`imagen_${imgId}.jpeg`);
+
+            const imagenData = fs.readFileSync(`imagen_${imgId}.jpeg`)
+            res.send(imagenData)
+
+    } catch (err) {
+        console.log(err)
+    }
+
+});
